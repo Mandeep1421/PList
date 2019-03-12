09d78ced5a7e7f2b1e9e7c97675383640d90a7dc
MacStudent <macstudent@126-imac16.local>
2019-03-12 19:51:39 -0400

    coding is done for plist in view controller

--- plist/ViewController.swift
+++ plist/ViewController.swift
@@ -16,10 +16,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     func readInformationPlist() {
-        if let bundlePath = Bundle.main.path(forResource: "SamplePropertyList", ofType: "plist") {
+        if let bundlePath = Bundle.main.path(forResource: "Employee", ofType: "plist") {
             let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
             
             print(dictionary!.description)
+            
+            print("--------Country List form Plist--------")
+             let countryList = dictionary!["Countaries"] as! NSArray
+            print(countryList[0])
+            for c in countryList
+            {
+                print(c)
+            }
+            
+            print("--------Employee List form Plist--------")
+            let employeeList = dictionary!["employees"] as! NSArray
+            for e in employeeList
+            {
+                let emp = e as! NSDictionary
+                print("Employee ID        : \(emp["eid"]!)")
+                print("Employee Name      : \(emp["ename"]!)")
+                print("Salary             : \(emp["Salary"]!)")
+                print("----------------------------------")
+            }
         }
     }
 
